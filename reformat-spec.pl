4f7a5e5663a8d436b1b02d194f59d8a7ad53df3a
protze@itc.rwth-aachen.de <protze1@llnl.gov>
2016-08-05 00:07:23 +0200

    fix missing \plc tags, that were missed before

--- reformat-spec.pl
+++ reformat-spec.pl
@@ -13,14 +13,26 @@ sub escapeUnderscore {
 
 sub trueaddplc{
   my $string = shift;
-  $string =~ s#([A-Za-z_][A-Za-z0-9_]*)(\s*,|\s*(?:/\*.*?\*/)?\s*$)#\\plc{${1}}${2}#g;
+  $string =~ s#([A-Za-z_][A-Za-z0-9_]*)(\s*,|\s*(?:/\*.*?\*/)?\s*\))#\\plc{${1}}${2}#g;
+  return $string;
+}
+
+sub trueaddplcblock{
+  my $string = shift;
+  $string =~ s#([A-Za-z_][A-Za-z0-9_]*)(\s*;)#\\plc{${1}}${2}#g;
   return $string;
 }
 
 
 sub addplc{
   my $string = shift;
-  $string =~ s/\(([^)]*)\)/"(".trueaddplc("${1}").")"/esg;
+  $string =~ s/(\((?>[^()]+|(?1))*\))/"".trueaddplc("${1}").""/esg;
+  return $string;
+}
+
+sub addplcstruct{
+  my $string = shift;
+  $string =~ s/(\{(?>[^{}]+|(?1))*\})/"".trueaddplcblock("${1}").""/esg;
   return $string;
 }
 
@@ -35,6 +47,6 @@ $string="";
 }
 
 $string =~ s/(\\verb(.))((?:(?!\2).)*)\2/"\\code{". escapeUnderscore("${3}") ."}"/esg;
-$string =~ s/(?:\\begin\{quote\}\s*)?\\begin\{verbatim\}\s*((?:(?!\\end).)*)\\end\{verbatim\}(?:\s*\\end\{quote\})?/"\\begin{boxedcode}\n". escapeUnderscore(addplc("${1}")) ."\\end{boxedcode}"/esg;
+$string =~ s/(?:\\begin\{quote\}\s*)?\\begin\{verbatim\}\s*((?:(?!\\end).)*)\\end\{verbatim\}(?:\s*\\end\{quote\})?/"\\begin{boxedcode}\n". escapeUnderscore(addplc(addplcstruct("${1}"))) ."\\end{boxedcode}"/esg;
 
 print $string
